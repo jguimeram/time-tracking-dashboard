5bc22bf3fb3164887afea28b8b8965872f2cbe8c
jguimeram <jguimera54@gmail.com>
2024-11-18 18:10:21 +0100

    fetch json

--- app/index.js
+++ app/index.js
@@ -1,2 +1,27 @@
-"use strict";
-console.log('hello, world');
+/* I love TypeScript */
+
+async function getData() {
+    const url = '../data/data.json'
+    try {
+
+        const response = await fetch(url)
+
+        if (!response.ok) throw new Error(`Response status: ${response.status}`)
+        
+        const json = await response.json()
+
+        setData(json)
+
+    } catch (error) {
+
+        console.log(error.message)
+    }
+    
+} 
+
+getData()
+
+async function setData(data) {
+   /* dynamic logic */
+}
+
